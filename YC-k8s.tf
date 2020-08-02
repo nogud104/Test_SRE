4e0c4bedd3f255241c7dd0d429769624dfcdd2ac
M.Chuvashov <raven4ster@gmail.com>
2020-08-02 14:32:48 +0300

    terraform add config deploy 3 nodes to yandexCloud

---
+++ YC-k8s.tf
@@ -0,0 +1,118 @@
+
+provider "yandex" {
+  token     = "AgAAAAAHE9jmAATuwQWS6LeQiEilr_MlU6qqj0w"
+  cloud_id  = "b1g2qi381jifub12uga3"
+  folder_id = "b1gnppu4ijh8tv2j7009"
+  zone      = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "k8s-node1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8cpqeflg039ibkphbo"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    ip_address = "10.0.0.8"
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/yandex-cloud-terraform/.ssh/yes.pub")}"
+  }
+}
+
+resource "yandex_compute_instance" "vm-2" {
+  name = "k8s-node2"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8cpqeflg039ibkphbo"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    ip_address = "10.0.0.9"
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/yandex-cloud-terraform/.ssh/yes.pub")}"
+  }
+}
+
+resource "yandex_compute_instance" "vm-3" {
+  name = "k8s-node3"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8cpqeflg039ibkphbo"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    ip_address = "10.0.0.10"
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/yandex-cloud-terraform/.ssh/yes.pub")}"
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["10.0.0.0/24"]
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+
+}
+
+output "internal_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+}
+
+output "internal_ip_address_vm_3" {
+  value = yandex_compute_instance.vm-3.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
+
+output "external_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+}
+
+output "external_ip_address_vm_3" {
+  value = yandex_compute_instance.vm-3.network_interface.0.nat_ip_address
+}
